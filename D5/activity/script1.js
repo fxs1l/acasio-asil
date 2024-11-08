1ae60a65e09a53dd3de6879099def125284b1988
Asil Andrei Acasio <fx.silndrei@gmail.com>
2024-11-08 21:17:07 +0800

    Day 3 javascript activities

---
+++ D5/activity/script1.js
@@ -0,0 +1,16 @@
+let score = parseInt(prompt("Enter score"));
+
+let grade;
+if (score >= 90) {
+    grade = "A";
+} else if (score >= 80 && score < 90) {
+    grade = "B";
+} else if (score >= 70 && score < 80) {
+    grade = "C";
+} else if (score >= 60 && score < 69) {
+    grade = "D";
+} else {
+    grade = "F";
+}
+
+console.log(grade);
---
+++ D5/activity/script2.js
@@ -0,0 +1,6 @@
+let starting = parseInt(prompt("Enter starting number"));
+let ending = parseInt(prompt("Enter ending number:"));
+
+for (let i = starting; i <= ending; i++) {
+    console.log(i);
+}
