5af1a5b3e2aeb91568ec4c9c6431bc1f9b1a7a67
Asil Andrei Acasio <fx.silndrei@gmail.com>
2024-11-11 19:02:40 +0800

    Add Day 5 lesson files

---
+++ D5/lesson/script2.js
@@ -0,0 +1,34 @@
+let age = 18;
+
+if (age >= 18) {
+    console.log("You are eligible to vote.");
+} else {
+    console.log("You are not eligible to vote.");
+}
+
+// Loops
+
+// while loop
+let count = 1;
+
+while (count <= 5) {
+    console.log("Count:", count);
+    count++;
+}
+
+// do while loop
+let i = 1;
+
+do {
+    console.log("Do while count:", i);
+    i++;
+} while (i <= 5);
+
+// for loop
+for (let i = 1; i <= 5; i++) {
+    console.log("Count:", i);
+}
+
+// alert("Hello");
+let number = parseInt(prompt("Enter Number"));
+alert(20 + number);
